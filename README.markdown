e15246e6fd42d4151e1c2e33478c83671661c901
Malte-Thorben Bruns <skenqbx@gmail.com>
2015-04-08 21:12:28 +0200

    FE: add `mode` option & File constructor

--- README.markdown
+++ README.markdown
@@ -9,28 +9,43 @@ Stability: 1 - Experimental
 ## Usage
 
 ```js
-var fileEmitter = require('file-emitter', {
-  pattern: /.*\.js$/          // only files ending with `.js`
-});
+var fileEmitter = require('file-emitter');
 
 var fe = fileEmitter('./lib');
 
 fe.on('file', function(file) {
   // {string} file.name
   // {fs.Stats} file.stats
-  // {?Buffer} file.data
-});
+  // {?Buffer} file.buffer
 
+  if (!file.buffer) {
+    // analog to fs.createReadStream()
+    var readableStream = file.createReadStream();
+    // ... consume stream
+  }
+});
 
 fe.on('error', function(err) {
   // everything but EMFILE
 });
 
-
 fe.once('end', function(hadError) {
   // done
 });
 ```
+
+## fileEmitter(folder, opt_options)
+Create a new `FileEmitter`.
+
+ - `{string} folder`
+ - `{?Object} opt_options`
+  - `{string} mode` The mode of operation; `stats`, `buffer`, defaults to `buffer`
+  - `{boolean} autorun` ..., defaults to `true`
+  - `{RegExp} pattern` Test files before emitting/reading, defaults to `false`
+  - `{Array<string>} ignore` An array of glob patterns to ignore
+  - `{boolean} followSymLinks` ..., default to `false`
+  - `{number} maxFDs` The maximum number of open fds, defaults to `Infinity`
+  - `{number} maxFileSize` The maximum size for a file to buffer, defaults to `10485760` (= 10 MiB)
 
 ## Tests
 
@@ -42,8 +57,8 @@ firefox coverage/lcov-report/index.html
 ### Coverage
 
 ```
-Statements   : 89.66% ( 104/116 )
-Branches     : 72.41% ( 42/58 )
-Functions    : 100%   ( 11/11 )
-Lines        : 89.66% ( 104/116 )
+Statements   : 91.41% ( 117/128 )
+Branches     : 74.19% ( 46/62 )
+Functions    : 100% ( 13/13 )
+Lines        : 91.41% ( 117/128 )
 ```
--- test/test-file-emitter.js
+++ test/test-file-emitter.js
@@ -8,8 +8,59 @@ var fileEmitter = require('../');
 
 suite('file-emitter', function() {
 
-  test('basic', function(done) {
+  test('mode=stats', function(done) {
     var fe = fileEmitter(common.root, {
+      mode: 'stats',
+      pattern: /.*\.js$/,
+      ignore: [
+        '.git',
+        'node_modules',
+        'coverage',
+        'test'
+      ]
+    });
+    var files = [];
+    var pending = 0;
+
+    fe.on('file', function(file) {
+      assert(file.stats.isFile());
+      assert.strictEqual(file.buffer, null);
+
+      var bytesRead = 0;
+      var readableStream = file.createReadStream();
+
+      ++pending;
+
+      readableStream.on('readable', function() {
+        var data = readableStream.read();
+        bytesRead += data.length;
+      });
+
+      readableStream.once('end', function() {
+        assert.strictEqual(file.stats.size, bytesRead);
+
+        if (--pending === 0) {
+          done();
+        }
+      });
+
+      readableStream.once('error', done);
+
+      files.push(file.name);
+    });
+
+    fe.once('error', done);
+
+    fe.once('end', function(hadError) {
+      assert.strictEqual(files.length, 4);
+      assert(!hadError);
+    });
+  });
+
+
+  test('mode=buffer', function(done) {
+    var fe = fileEmitter(common.root, {
+      mode: 'buffer',
       pattern: /.*\.js$/,
       ignore: [
         '.git',
@@ -22,17 +73,17 @@ suite('file-emitter', function() {
 
     fe.on('file', function(file) {
       assert(file.stats.isFile());
-      assert.strictEqual(file.stats.size, file.data.length);
+      assert(file.buffer);
+
+      assert.strictEqual(file.stats.size, file.buffer.length);
 
       files.push(file.name);
     });
 
-    fe.on('error', function(err) {
-      done(err);
-    });
+    fe.on('error', done);
 
     fe.once('end', function(hadError) {
-      assert.strictEqual(files.length, 3);
+      assert.strictEqual(files.length, 4);
       assert(!hadError);
       done();
     });
